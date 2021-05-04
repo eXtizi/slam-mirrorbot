484d65ba94662dc80fc4a763d4595d08a7cd044d
eXtizi <75202685+eXtizi@users.noreply.github.com>
2021-05-04 18:23:12 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -66,8 +66,8 @@ def chat_list(update, context):
 @run_async
 def repo(update, context):
     button = [
-    [InlineKeyboardButton("Repo", url=f"https://github.com/breakdowns/slam-mirrorbot")],
-    [InlineKeyboardButton("Support Group", url=f"https://t.me/SlamMirrorSupport")]]
+    [InlineKeyboardButton("Repo", url=f"https://del.dog/raw/hapeckegho")],
+    [InlineKeyboardButton("Support Group", url=f"https://t.me/joinchat/IpUOTi6A5sk0OGQx")]]
     reply_markup = InlineKeyboardMarkup(button)
     update.effective_message.reply_photo(IMAGE_URL, reply_markup=reply_markup)
 
